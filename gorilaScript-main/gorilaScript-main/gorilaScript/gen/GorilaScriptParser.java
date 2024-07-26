6a3a9db7e19fc509f7bb1e11a932a68bd483552a
Gustavo <105179924+gustavoapc@users.noreply.github.com>
2024-07-26 02:46:26 +0000

    Teste de código

--- gorilaScript-main/gorilaScript-main/gorilaScript/gen/GorilaScriptParser.java
+++ gorilaScript-main/gorilaScript-main/gorilaScript/gen/GorilaScriptParser.java
@@ -1,4 +1,4 @@
-// Generated from .//GorilaScript.g4 by ANTLR 4.13.1
+// Generated from GorilaScript.g4 by ANTLR 4.13.1
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.*;
