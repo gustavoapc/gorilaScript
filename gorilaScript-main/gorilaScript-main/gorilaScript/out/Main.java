942a1c8dae78de451c6a2276d0476d46939bb790
Caio <92593488+prokofitch@users.noreply.github.com>
2024-07-27 19:51:19 +0000

    Add class to work with our language

---
+++ gorilaScript-main/gorilaScript-main/gorilaScript/out/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor inicial: ");
+        float valorInicial = scanner.nextFloat();
+
+        System.out.print("Digite a taxa de juros (em %): ");
+        float taxaJuros = scanner.nextFloat();
+
+        System.out.print("Digite o número de anos: ");
+        int anos = scanner.nextInt();
+
+        System.out.print("Digite o valor do aporte mensal: ");
+        float aporteMensal = scanner.nextFloat();
+
+        float valorFinal = GeneratedCode.calcularInvestimento(valorInicial, taxaJuros, anos, aporteMensal);
+
+        System.out.println("O valor final do investimento é: " + valorFinal);
+
+        scanner.close();
+    }
+}
