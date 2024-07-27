fbc34d0ffa9107409d2b96ff651a9de4df2228e5
Caio <92593488+prokofitch@users.noreply.github.com>
2024-07-27 22:14:20 +0000

    Fix CLI

--- gorilaScript/out/Main.java
+++ gorilaScript/out/Main.java
@@ -1,25 +1,32 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            try {
+                System.out.print("Digite o valor inicial: ");
+                float valorInicial = scanner.nextFloat();
 
-        System.out.print("Digite o valor inicial: ");
-        float valorInicial = scanner.nextFloat();
+                System.out.print("Digite a taxa de juros (em %): ");
+                float taxaJuros = scanner.nextFloat();
 
-        System.out.print("Digite a taxa de juros (em %): ");
-        float taxaJuros = scanner.nextFloat();
+                System.out.print("Digite o número de anos: ");
+                int anos = scanner.nextInt();
 
-        System.out.print("Digite o número de anos: ");
-        int anos = scanner.nextInt();
+                System.out.print("Digite o valor do aporte mensal: ");
+                float aporteMensal = scanner.nextFloat();
 
-        System.out.print("Digite o valor do aporte mensal: ");
-        float aporteMensal = scanner.nextFloat();
+                float valorFinal = GeneratedCode.calcularInvestimento(valorInicial, taxaJuros, anos, aporteMensal);
 
-        float valorFinal = GeneratedCode.calcularInvestimento(valorInicial, taxaJuros, anos, aporteMensal);
-
-        System.out.println("O valor final do investimento é: " + valorFinal + " R$");
+                System.out.printf("O valor final do investimento é: %.2f R$\n", valorFinal);
+                break;
+            } catch (Exception e) {
+                System.out.println("\nErro: Por favor, insira apenas números.\nPara valores decimais, utilize '.' (ponto) como separador.\nExemplo: 10.50\nVamos Recomeçar!\n\n");
+                scanner.nextLine();
+            }
+        }
 
         scanner.close();
     }
